c0b1ab2695e501afffb50ea53c3eb7fb0d1c5ffd
Ryan Jeynes <jeynesrya@gmail.com>
2019-11-10 22:12:23 +0000

    fixed lint error

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
-      return p.id == id;
+      return p.id === id;
     });
 
     // make copies, not references!
