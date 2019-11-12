c6879a7c0ce37e7e9240d9e476add1b12fbdcfad
Ryan Jeynes <jeynesrya@gmail.com>
2019-11-12 23:13:35 +0000

    completed upto lecture 96

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import classes from './Cockpit.module.css';
 
-const cockpit = (props) => {
+const Cockpit = (props) => {
+
+    // only runs the first time
+    useEffect(() => {
+      console.log('[Cockput.js] useEffect');
+      // Http request...
+      setTimeout(() => {
+        alert('Saved data to cloud start!');
+      }, 1000);
+      return () => {
+        //runs before main useEffect function but after the (first) render cycle
+        console.log('[Cockpit.js] cleanup work in useEffect')
+      }
+    }, []);
+
+    // only execute when persons changed; can have multiple useEffect
+    // useEffect(() => {
+    //   console.log('[Cockput.js] useEffect');
+    //   // Http request...
+    //   setTimeout(() => {
+    //     alert('Saved data to cloud!');
+    //   }, 1000);
+    // }, [props.persons]);
+
     const assignedClasses = [];
     let btnClass = '';
 
@@ -28,4 +51,4 @@ const cockpit = (props) => {
     );
 };
 
-export default cockpit;
+export default Cockpit;
