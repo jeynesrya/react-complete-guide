2a0c83a0cd2c870fda7773d46434e06186c870ce
Ryan Jeynes <jeynesrya@gmail.com>
2019-11-12 16:44:51 +0000

    adding new application structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.module.css';
-import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -50,42 +50,23 @@ class App extends Component {
   // gets called whenever a state change occurs.
   render() {
     let persons = null;
-    let btnClass = '';
 
     // recommended way for return conditional list(s)
     if (this.state.showPersons) {
       persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}>
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                changed={(event) => this.nameChangedHandler(event, person.id)}
-            /></ErrorBoundary>
-          })}
-        </div>
+        <Persons 
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler} />
       );
-      // change style dynamically
-      btnClass = classes.Red;
-    }
-
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red); // classes = ['red']
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     return (
       <div className={classes.App}>
-        <h1>Hi, I'm a React App</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button
-          className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons
-        </button>
+        <Cockpit 
+          showPersons={this.state.showPersons} 
+          persons={this.state.persons} 
+          clicked={this.togglePersonsHandler} />
         {persons}
       </div>
     );
